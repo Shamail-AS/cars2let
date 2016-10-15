a68c750ffa3a8cc6b068ada5a2a34a4ae34ca2ab
Shamail Ahmed Siddiqui <shamail.siddiqui.13@ucl.ac.uk>
2016-10-15 16:08:50 +0100

    Added GPS http://gpslive.co.uk/ iframe to investor home

--- resources/views/investor/home.blade.php
+++ resources/views/investor/home.blade.php
@@ -8,7 +8,7 @@
 
     <div class="flex-container">
         <div class="map-container">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19846.006685263703!2d-1.7580300999999998!3d51.55446775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1453767116129"
+            <!--<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19846.006685263703!2d-1.7580300999999998!3d51.55446775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1453767116129"
                     width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
             <div class="sidebar sidebar-left">
                 <div class="sidebar-item">
@@ -33,7 +33,9 @@
                     <p class="subtitle">London,SW14 7NJ</p>
                 </div>
 
-            </div>
+            </div>-->
+            <iframe src="http://gpslive.co.uk/"
+                    sandbox="allow-forms allow-same-origin allow-pointer-lock allow-scripts allow-top-navigation"></iframe>
         </div>
         <div class="revenue-container">
 
